833a7583a7e6487b2937cf2b894074fcade39728
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2022-03-17 17:00:35 +0200

    Add example of use

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,25 @@
+import { useState } from 'react'
+import { Modal } from './modal'
+
+const Body = ({setShowModal} : any) => {
+  return (
+    <>
+      <p>
+        In order to close the modal, you can click outside of it. Also, you can click the button
+        on the top right corner or click this button below
+      </p>
+      <button onClick={() => setShowModal(false)}>Click me!</button>
+    </>
+  )
+}
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false)
+
+  const handleShowModal = () => {
+    setShowModal(true)
+  }
+
   return (
     <>
       <h1>Trabe interview</h1>
@@ -7,9 +27,18 @@ const App = () => {
 
       <hr />
 
-    </>
+      <button onClick={handleShowModal}>Show modal</button>
 
-  );
+      <Modal 
+        showModal={showModal} 
+        setShowModal={setShowModal}
+        title='Hello Trabe'
+        Body={() => (<Body setShowModal={setShowModal} />)}
+        footer='Developed by Jesús González Álvarez'
+        rounded
+      />
+    </>
+  )
 }
 
-export default App;
+export default App
